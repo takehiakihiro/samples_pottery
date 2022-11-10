b45f61d27da88f9c92e202848e3b343d9b2c338a
takehiakihiro <akihiro2@gmail.com>
2022-11-10 14:49:35 +0900

    fixed a bug

--- udp_queue.c
+++ udp_queue.c
@@ -18,17 +18,16 @@ typedef struct _udp_elem {
 } udp_elem_t;
 
 
-#define POTTERY_PAGER_PREFIX udp_queue
-#define POTTERY_PAGER_VALUE_TYPE udp_elem_t*
-#define PPOTTERY_PAGER_PER_PAGE 256
-#define POTTERY_PAGER_LIFECYCLE_MOVE_BY_VALUE 1
-#include "pottery/pager/pottery_pager_static.t.h"
+#define POTTERY_NODE_LIST_PREFIX udp_queue
+#define POTTERY_NODE_LIST_VALUE_TYPE udp_elem_t*
+#define POTTERY_NODE_LIST_LIFECYCLE_BY_VALUE 1
+#include "pottery/node_list/pottery_node_list_static.t.h"
 
 
 /**
  *
  */
-udp_elem_t* udp_queue_value_set(uint32_t org_addr, uint16_t org_port, uint32_t appl_addr, uint16_t appl_port, uint8_t* md5)
+udp_elem_t* udp_queue_new(uint32_t org_addr, uint16_t org_port, uint32_t appl_addr, uint16_t appl_port, uint8_t* md5)
 {
   udp_elem_t* elem = (udp_elem_t*)malloc(sizeof(udp_elem_t));
   elem->org_addr = org_addr;
@@ -37,6 +36,8 @@ udp_elem_t* udp_queue_value_set(uint32_t org_addr, uint16_t org_port, uint32_t a
   elem->appl_port = appl_port;
   memcpy(elem->md5, md5, 16);
   elem->created_at = time(NULL);
+  printf("insert:  elem=%p, %08x:%04x, %08x:%04x\n", elem, elem->org_addr, elem->org_port, elem->appl_addr, elem->appl_port);
+  return elem;
 }
 
 
@@ -51,31 +52,23 @@ int main(void)
   udp_queue_init(&client_to_server_queue);
 
   for (uint32_t i = 0; i < 10; i++) {
-    udp_queue_insert_last(&client_to_server_queue, udp_queue_value_set(0x0a0a0a0a + i, 8080 + i, 0x0b0b0b0b + i, 7890 + i, md5));
-    printf("inserted\n");
+    udp_queue_insert_last(&client_to_server_queue, udp_queue_new(0x0a0a0a0a + i, 8080 + i, 0x0b0b0b0b + i, 7890 + i, md5));
   }
 
-  udp_queue_entry_t first = udp_queue_first(&client_to_server_queue);
-
-#if 1
-  // Print its contents
-  for (uint32_t i = 0; i < udp_queue_count(&client_to_server_queue); ++i) {
-    printf("%u\n", i);
-    udp_elem_t* it = *udp_queue_entry_ref(&client_to_server_queue, udp_queue_at(&client_to_server_queue, i));
-    printf("it=%p\n", it);
-    // printf("%08x:%04x, %08x:%04x\n", it->org_addr, it->org_port, it->appl_addr, it->appl_port);
+  udp_elem_t** it = udp_queue_begin(&client_to_server_queue);
+  udp_elem_t** next;
+  udp_elem_t** end = udp_queue_end(&client_to_server_queue);
+  printf("begin it=%p, end=%p\n", it, end);
+  while (it != end) {
+    udp_elem_t* elem = *it;
+    printf("elem=%p\n", elem);
+    printf("extract: elem=%p, %08x:%04x, %08x:%04x\n", elem, elem->org_addr, elem->org_port, elem->appl_addr, elem->appl_port);
+    next = udp_queue_next(&client_to_server_queue, it);
+    printf("next, it=%p\n", next);
+    udp_queue_remove(&client_to_server_queue, it);
+    printf("removed\n");
+    it = next;
   }
-#else
-
-  // udp_queue_remove(&client_to_server_queue);
-  udp_queue_entry_t it = udp_queue_begin(&client_to_server_queue);
-  printf("begin\n");
-  while (it != NULL) {
-    printf("%08x:%04x, %08x:%04x\n", (*it)->org_addr, (*it)->org_port, (*it)->appl_addr, (*it)->appl_port);
-    it = udp_queue_next(&client_to_server_queue, it);
-    printf("next\n");
-  }
-#endif
 
   udp_queue_destroy(&client_to_server_queue); // frees all ints
 }
--- udp_queue_loop.c
+++ udp_queue_loop.c
@@ -20,7 +20,7 @@ typedef struct _udp_elem {
 
 
 #define POTTERY_NODE_LIST_PREFIX udp_queue
-#define POTTERY_NODE_LIST_VALUE_TYPE udp_elem_t
+#define POTTERY_NODE_LIST_VALUE_TYPE udp_elem_t*
 #define POTTERY_NODE_LIST_LIFECYCLE_BY_VALUE 1
 #include "pottery/node_list/pottery_node_list_static.t.h"
 
@@ -28,45 +28,80 @@ typedef struct _udp_elem {
 /**
  *
  */
-void udp_queue_value_set(udp_elem_t* elem, uint32_t org_addr, uint16_t org_port, uint32_t appl_addr, uint16_t appl_port, uint8_t* md5)
+udp_elem_t* udp_queue_new(uint32_t org_addr, uint16_t org_port, uint32_t appl_addr, uint16_t appl_port, uint8_t* md5)
 {
+  udp_elem_t* elem = (udp_elem_t*)malloc(sizeof(udp_elem_t));
   elem->org_addr = org_addr;
   elem->org_port = org_port;
   elem->appl_addr = appl_addr;
   elem->appl_port = appl_port;
   memcpy(elem->md5, md5, 16);
   elem->created_at = time(NULL);
+  // printf("inserted: elem=%p, %08x:%04x, %08x:%04x\n", elem, elem->org_addr, elem->org_port, elem->appl_addr, elem->appl_port);
+  return elem;
 }
 
 
 /**
  *
  */
-int main(void)
+int main(int argc, char* argv[])
 {
   uint8_t md5[16];
   uint32_t insert_max;
+  pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+  udp_elem_t** it;
+  udp_elem_t** next;
+  udp_elem_t** end;
 
   insert_max = strtoul(argv[1], NULL, 10);
 
   udp_queue_t client_to_server_queue;
   udp_queue_init(&client_to_server_queue);
 
+  udp_queue_insert_last(&client_to_server_queue, udp_queue_new(0x0b0a0a0a, 9080, 0x0c0b0b0b, 2890, md5));
+
   for (uint32_t i = 0; i < insert_max; i++) {
-    udp_elem_t* elem;
-    udp_queue_emplace_last(&client_to_server_queue, &elem);
-    udp_queue_value_set(elem, 0x0a0a0a0a + i, 8080 + (i % 100), 0x0b0b0b0b + i, 7890 + (i % 100), md5);
-    //printf("inserted\n");
+    pthread_mutex_lock(&mtx);
+    udp_queue_insert_last(&client_to_server_queue, udp_queue_new(0x0a0a0a0a + i, 8080 + (i % 100), 0x0b0b0b0b + i, 7890 + (i % 100), md5));
+    pthread_mutex_unlock(&mtx);
+
+    pthread_mutex_lock(&mtx);
+    {
+      it = udp_queue_begin(&client_to_server_queue);
+      end = udp_queue_end(&client_to_server_queue);
+      it = udp_queue_next(&client_to_server_queue, it);
+      if (it != end) {
+        udp_queue_remove(&client_to_server_queue, it);
+      }
+    }
+    pthread_mutex_unlock(&mtx);
   }
 
-  udp_elem_t* it = udp_queue_begin(&client_to_server_queue);
+#if 0
+  it = udp_queue_begin(&client_to_server_queue);
+  end = udp_queue_end(&client_to_server_queue);
   // printf("begin\n");
-  while (it != NULL) {
-    // printf("%08x:%04x, %08x:%04x\n", it->org_addr, it->org_port, it->appl_addr, it->appl_port);
-    it = udp_queue_next(&client_to_server_queue, it);
-    break;
+  while (it != end) {
+    udp_elem_t* elem = *it;
+    // printf("extract: elem=%p, %08x:%04x, %08x:%04x\n", elem, elem->org_addr, elem->org_port, elem->appl_addr, elem->appl_port);
     // printf("next\n");
+    next = udp_queue_next(&client_to_server_queue, it);
+    udp_queue_remove(&client_to_server_queue, it);
+    it = next;
   }
+
+#endif
+
+  it = udp_queue_begin(&client_to_server_queue);
+  end = udp_queue_end(&client_to_server_queue);
+  while (it != end) {
+    udp_elem_t* elem = *it;
+    printf("extract: elem=%p, %08x:%04x, %08x:%04x\n", elem, elem->org_addr, elem->org_port, elem->appl_addr, elem->appl_port);
+    // printf("next\n");
+    it = udp_queue_next(&client_to_server_queue, it);
+  }
+
 
   udp_queue_destroy(&client_to_server_queue); // frees all ints
 }
